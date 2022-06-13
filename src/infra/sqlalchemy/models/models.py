ec7e25b5fb6c96080ec5b0b195b1abb007064428
robertomacedo <macedorss20@gmail.com>
2022-06-13 20:14:12 -0300

    Adicionado o modelo produto

--- src/infra/sqlalchemy/models/models.py
+++ src/infra/sqlalchemy/models/models.py
@@ -1,2 +1,13 @@
-class Produto():
-    pass
+from sqlalchemy import Column, Integer, String, Boolean
+from src.infra.sqlalchemy.config.database import Base 
+
+
+class Produto(Base):
+
+    __tablename__ = 'produto'
+
+    id = Column(Integer, primary_key=True, index=True)
+    nome = Column(String)
+    detalhe = Column(String)
+    preco = Column(Integer)
+    disponivel = Column(Boolean)
---
+++ src/infra/sqlalchemy/repositorio/repo_produto.py
@@ -0,0 +1,31 @@
+from sqlalchemy.orm import Session 
+from src.schemas import schems
+from src.infra.models import models
+
+class RepoProduto():
+    def __init__(self, db: Session):
+        self.db = db
+
+
+    def criar(self, produto: schemas.Produto):
+        db_produto = models.Produto(nome=produto.nome,
+                                    detalhe=produto.detalhe,
+                                    preco=produto.preco,
+                                    disponivel=produto.disponivel)
+        self.db.add(db_produto) 
+        self.db.commint()
+        self.db.refresh(db_produto)
+        return db_produto
+
+
+    def lestar(self):
+        produtos = self.db.query(models.Produto).all()
+        return produtos
+
+
+    def obter(self):
+        pass 
+
+
+    def remover(self):
+        pass
