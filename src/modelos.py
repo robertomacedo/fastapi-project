d9edec451a6e37774f4f2bf5d72a1dddd5b735d8
robertomacedo <macedorss20@gmail.com>
2022-06-13 19:25:19 -0300

    fasteapi modelos criados

---
+++ src/modelos.py
@@ -0,0 +1,31 @@
+#from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional, List
+
+
+class Usuario(BaseModel):
+    id: Optional[str] = None
+    nome: str
+    telefone: str
+    my_produtos: List[Produto]
+    my_vendas: List[Pedido]
+    my_pedidos: List[Pedido]
+
+
+class Produto(BaseModel):
+    id: Optional[str] = None
+    usuario: Usuario
+    nome: str
+    detalhe: str
+    preco: float
+    disponivel: bool = False
+
+
+class Pedido(BaseModel):
+    id: Optional[str] = None
+    usuario: Usuario
+    produto: Produto
+    quantidade: int
+    entrega: bool = True
+    local: str
+    obs: Optional[str] = 'Sem observações'
